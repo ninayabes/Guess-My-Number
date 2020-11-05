2fd699a47afd63e26d5a195b544e2683ed8734aa
Nina Yabes <ninacariza.yabes@gmail.com>
2020-11-04 17:29:55 -0800

    minor CSS changes

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     displayMessage(' INVALID ENTRY!');
     document.querySelector('.message').style.fontSize = '3rem';
+    document.querySelector('.message').style.color = '#8E0707';
   } else if (guess === secretNumber) {
     displayMessage('YOU WIN!');
+    document.querySelector('.message').style.fontSize = '3rem';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#078E52';
-    document.querySelector('.message').style.fontSize = '3rem';
+    document.querySelector('.message').style.color = '#FFDF00';
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highScore) {
@@ -29,12 +31,14 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess !== secretNumber) {
     if (score > 1) {
       displayMessage(guess > secretNumber ? ' TOO HIGH!' : ' TOO LOW!');
+      document.querySelector('.message').style.color = '#8E0707';
+      document.querySelector('.message').style.fontSize = '3rem';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       displayMessage('YOU LOST!');
-      document.querySelector('body').style.backgroundColor = '#8E0707';
       document.querySelector('.message').style.fontSize = '3rem';
+      document.querySelector('.message').style.color = '#8E0707';
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -44,6 +48,8 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   displayMessage('Start guessing...');
+  document.querySelector('.message').style.fontSize = '2rem';
+  document.querySelector('.message').style.color = '#eee';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
